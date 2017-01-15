e2fdb60e6353d1395cad5433e8f0dce8dc652139
Samuel Cochran <sj26@sj26.com>
2017-01-16 10:44:13 +1100

    Update rails aliases

--- .zsh/rc.d/aliases.zsh
+++ .zsh/rc.d/aliases.zsh
@@ -1,31 +1,6 @@
-rails_version() {
-  which rails >/dev/null && rails -v 2>/dev/null | sed 's/Rails //'
-}
-
-r() {
-  local name="$1"
-  shift
-  if [[ -z "$name" ]]; then
-    echo "Usage: $0 command *args" >&2
-    return 1
-  fi
-  if [[ -x "./script/$name" ]]; then
-    ./script/$name $@
-  elif [[ -x "./script/rails" ]]; then
-    ./script/rails "$name" $@
-  elif [[ -n "$(rails_version | grep '^3')" ]]; then
-    rails "$name" $@
-  else
-    echo "Please change to the root of your project first." >&2
-    return 1
-  fi
-}
-
-# Time saving shortcuts
-#alias rs="r server"
-alias rs="echo Aren't you using pow?!"
+alias r=rails
+alias rs="r server"
 alias rc="r console"
-alias rp='r plugin'
 alias rg='r generate'
 alias rd="r dbconsole"
 alias rr='mkdir -p tmp && touch tmp/restart.txt'
