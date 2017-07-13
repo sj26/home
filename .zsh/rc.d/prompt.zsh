dcc0dab75790fa6d06b2b0cf9de86f486b326fdc
Samuel Cochran <sj26@sj26.com>
2017-07-13 11:11:22 +1000

    One day I'll async...

--- .zsh/rc.d/prompt.zsh
+++ .zsh/rc.d/prompt.zsh
@@ -4,16 +4,19 @@ prompt_git() {
     if [[ "$(git status --porcelain 2> /dev/null)" != "" ]]; then
       branch="$branch$txtred*$txtblu"
     fi
-    echo " %F{blue}($branch)%f"
+    PS1_GIT=" %F{blue}($branch)%f"
+  else
+    PS1_GIT=""
   fi
 }
+precmd () { prompt_git }
 
 # Perform parameter & command substitution in prompt
 setopt PROMPT_SUBST
 
-PROMPT="%1/\$(prompt_git) %(?.%F{green}.%F{red})%(#.#.%%)%f "
+PS1="%1/\$PS1_GIT %(?.%F{green}.%F{red})%#%f "
 
 # If we're on a remote connection, prefix with host details
 if [[ -n "$SSH_CONNECTION" ]]; then
-  PROMPT="%n@%m $PROMPT"
+  PS1="%n@%m $PROMPT"
 fi
